93dd53dc8bc1433e4799fd5c2886633553f75471
Hiroyuki Tamae <e165759@ie.u-ryukyu.ac.jp>
2016-12-08 14:23:18 +0900

    First Commit : Add the file.

---
+++ src/Report5.java
@@ -0,0 +1,10 @@
+/**
+ * Created by e165759 on 2016/12/08.
+ **/
+public class Report5 {
+    public static void main(String[] args){
+        String str = null;
+
+        System.out.println(str.length());
+    }
+}
