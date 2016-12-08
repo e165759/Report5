4abc5afe77db08c11630e9b1cb1ef1dae942a7a3
Hiroyuki Tamae <e165759@ie.u-ryukyu.ac.jp>
2016-12-08 15:14:44 +0900

    Second Commit : Add the exception.

--- src/Report5.java
+++ src/Report5.java
@@ -3,8 +3,20 @@
  **/
 public class Report5 {
     public static void main(String[] args){
-        String str = null;
 
-        System.out.println(str.length());
+        /* 想定している例外が起きる箇所では、予め例外に対応した処理を記述する。
+            try{ 例外が起きる箇所 }
+            catch(想定してる例外){例外処理}
+            finally{例外の有無にかかわらず、最終的に実行させたい処理}
+         */
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("例外発生！");
+            e.printStackTrace();
+            System.exit(1); //exit code を1に。(正常終了 = 0)
+        }
+
     }
 }
